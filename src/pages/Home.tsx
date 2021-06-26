57b016d4a775a2d25010c2aebddd50e871e844db
thalesousa <thalestjsb@gmail.com>
2021-06-25 22:15:04 -0300

    Landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,6 +36,12 @@ export function Home() {
       alert('Room does not exists.');
       return;
     }
+
+    if (roomRef.val().endedAt) {
+      alert('Room already closed.');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
